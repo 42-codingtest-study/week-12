c8ad2d80a2be904b94c6a990cd4b4d85c434f2d1
Chanwoong1 <jcw7468@gmail.com>
2023-02-10 00:04:27 +0900

    feat: 16400

--- 16400.py
+++ 16400.py
@@ -8,12 +8,10 @@ for i in range(2, int(N ** 0.5) + 1) :
     if prime[i] :
         for j in range(2 * i, N + 1, i) :
             prime[j] = False
-
+prime_lst = [i for i in range(N + 1) if prime[i]]
 dp = [0] * (N + 1)
-for i in range(1, N + 1) :
-    if prime[i] :
-        dp[i] = dp[i - 1] + 1
-    else :
-        dp[i] = dp[i - 1] % 123456789
+dp[0] = 1
+for i in prime_lst :
+    for j in range(i, N + 1) :
+            dp[j] += dp[j - i] % 123456789
 print(dp[-1] % 123456789)
-print(dp)
